fd5326d1c721941083de3577493a6b3ecae29f1c
redouane <redouane.sobaihi@live.com>
2025-01-11 21:17:08 +0100

    feat: update social list styling

--- src/components/social.jsx
+++ src/components/social.jsx
@@ -1,21 +1,38 @@
 export default function Social() {
   return <>
-    <section className="social">
-      <a href="https://github.com/Redouane64" target="_blank">
-        <img className="icon" src="/github.svg" />
-      </a>
-      <a href="https://stackoverflow.com/users/1690931/redouane" target="_blank">
-        <img className="icon" src="/stack-overflow.svg" />
-      </a>
-      <a href="https://www.linkedin.com/in/redouane-s-6b7762281/" target="_blank">
-        <img className="icon" src="/linkedin.svg" />
-      </a>
-      <a href="https://twitter.com/__redouane" target="_blank">
-        <img className="icon" src="/x-twitter.svg" />
-      </a>
-      <a href="mailto:redouane.sobaihi0@gmail.com" target="_blank">
-        <img className="icon" src="/email.svg" />
-      </a>
+    <section>
+      <ul className="social">
+        <li>
+          <a href="https://github.com/Redouane64" target="_blank">
+            <img className="icon" src="/github.svg" />
+            github/Redouane64
+          </a>
+        </li>
+        <li>
+          <a href="https://stackoverflow.com/users/1690931/redouane" target="_blank">
+            <img className="icon" src="/stack-overflow.svg" />
+            stackoverflow/redouane
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/redouane-s-6b7762281/" target="_blank">
+            <img className="icon" src="/linkedin.svg" />
+            @redouane
+          </a>
+        </li>
+        <li>
+          <a href="https://twitter.com/__redouane" target="_blank">
+            <img className="icon" src="/x-twitter.svg" />
+            @__redouane
+          </a>
+        </li>
+        <li>
+          <a href="mailto:redouane.sobaihi0@gmail.com" target="_blank">
+            <img className="icon" src="/email.svg" />
+            redouane.sobaihi0@gmail.com
+          </a>
+        </li>
+      </ul>
     </section>
   </>
 }
