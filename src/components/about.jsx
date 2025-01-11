93ab39d7e5922a386d9444fff04809ae9b57da30
redouane <redouane.sobaihi@live.com>
2025-01-11 22:44:16 +0100

    feat: prettify bio text

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -31,7 +31,7 @@ export function About() {
   ];
   return <>
     <section>
-      <article>
+      <article className="bio">
         <h3>About Me</h3>
         <p>
         Hello, I’m Redhouane <span>(/rə-ˈdwɑːn/)</span>. I’m a software engineer with a degree from Tomsk State University. My work focuses on crafting reliable, scalable software systems that solve real-world problems. I enjoy learning new technologies, refining my skills, and contributing to meaningful projects. Outside of work, I’m always curious about exploring new ideas and improving my approach to software development.
