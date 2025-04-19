cdcb92ba0ccbb1dec51abb9064152c2aa8264706
redouane <redouane.sobaihi@live.com>
2025-04-19 16:41:10 +0200

    feat: add flag to enable/disable projects

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,26 +1,41 @@
-import projects  from '../data/projects.json';
+import projects from "../data/projects.json";
 
 export function Project({ name, description, url }) {
-  return <>
-    <div className='project-header'>
-      <h4>{name}</h4>
-      <a href={url}>View Project</a>
-    </div>
-    <p className='project-description'>{description}</p>
-  </>
+  return (
+    <>
+      <div className="project-header">
+        <h4>{name}</h4>
+        <a href={url}>View Project</a>
+      </div>
+      <p className="project-description">{description}</p>
+    </>
+  );
 }
 
 export default function ListProjects() {
-  return <>
-    <section>
-      <h3>
-        Projects
-      </h3>
-      <div>
-        {
-          Object.entries(projects).map(([name, details]) => <Project key={name} name={name} description={details.description} url={details.url} />)
-        }
-      </div>
-    </section>
-  </>
+  const list = Object.entries(projects).filter(
+    ([_, details]) => details.enabled
+  );
+
+  return (
+    <>
+      <section>
+        {list.length > 0 && (
+          <>
+            <h3>Projects</h3>
+            <div>
+              {list.map(([name, details]) => (
+                <Project
+                  key={name}
+                  name={name}
+                  description={details.description}
+                  url={details.url}
+                />
+              ))}
+            </div>
+          </>
+        )}
+      </section>
+    </>
+  );
 }
